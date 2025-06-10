ba6543dae6ae0f9deae6c4132212a62bd4f5f34a
Tomderandere <tompbickle@gmail.com>
2025-06-10 09:19:58 +0200

    Aufgabe 1

--- praxis/aufgaben/aufgabe1/aufgabe1.cpp
+++ praxis/aufgaben/aufgabe1/aufgabe1.cpp
@@ -7,6 +7,19 @@
 // Prüft, ob die Werte in v in absteigender Reihenfolge sortiert sind.
 bool is_sorted_ascending(std::vector<int> const& v)
 {
-    // TODO
-    return false;
+   if (v.size()==0)
+   {
+    return true;
+   }
+   
+    for (size_t i = 0; i < v.size()-1; i++)
+    {
+        if (v[i]>v[i+1])
+        {
+           return false;
+        }
+        
+    }
+    
+    return true;
 }
