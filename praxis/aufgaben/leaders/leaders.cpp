3a0429dfb682c1e9694464a7de066dc3dbc00863
Tomderandere <tompbickle@gmail.com>
2025-06-15 11:16:01 +0200

    add leaders

---
+++ praxis/aufgaben/leaders/leaders.cpp
@@ -0,0 +1,14 @@
+
+
+#include "vector"
+
+std::vector<int> leaders( const std::vector<int> & list)
+{
+std::vector<int> result = {};
+for (size_t i = list.size(); i=0 ; i--)
+{
+   /* code */
+}
+
+return result;
+}
---
+++ praxis/aufgaben/leaders/leaders_tests.cpp
@@ -0,0 +1,34 @@
+#include <catch2/catch_test_macros.hpp>
+#include <vector>
+#include <algorithm>
+#include <leaders.cpp>
+
+TEST_CASE("find leaders") {
+    std::vector<int> input{5, 3, 8, 2, 7, 4};
+    std::vector<int> expected{8, 7, 4};
+
+    auto result = leaders(input);
+
+
+    REQUIRE(result == expected);
+}
+
+TEST_CASE("find leaders in order") {
+    std::vector<int> input{1, 2, 3, 4, 5};
+    std::vector<int> expected{5};
+
+    auto result = leaders(input);
+
+
+    REQUIRE(result == expected);
+}
+
+TEST_CASE("find leaders doppelt") {
+    std::vector<int> input{5, 4, 3, 4, 1};
+    std::vector<int> expected{5, 4, 4, 1};
+
+    auto result = leaders(input);
+
+
+    REQUIRE(result == expected);
+}
